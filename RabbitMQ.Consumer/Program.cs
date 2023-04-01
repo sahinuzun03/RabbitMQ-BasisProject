9c1e58cb6cf6b21b4ec8b93fc4806ff72d8b66fb
uzunsahin <sahin.uzun@gratas.com.tr>
2023-04-01 23:21:15 +0300

    Add project files.

---
+++ RabbitMQ.Consumer/Program.cs
@@ -0,0 +1,35 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+
+//Bağlantı oluşturuldu
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://agynxcdk:czCiBq-QnX_MgyDat8Iqxl2bVtBOGZFH@woodpecker.rmq.cloudamqp.com/agynxcdk");
+
+//Bağlantı Aktifleştirme ve Kanal Açma
+
+using IConnection connection = factory.CreateConnection(); //IDisposable olduğu için using koydum ki kendisini patlatması için.
+using IModel channel = connection.CreateModel(); //IDisposable olduğu için using koydum ki kendisini patlatması için.
+
+//Queue Oluşturm
+channel.QueueDeclare(queue: "example-queue", exclusive: false);
+//Consumer tarafında da kuyruk aynı yapı ile tanımlanmalıdır.
+
+EventingBasicConsumer consumer = new(channel);
+//Mesajı okumaya dnlemeye başlıyoruz
+//AutoAck mesaj silinecek mi vs
+//consumer en sondaki mesajı tüketecek olan kişi
+channel.BasicConsume(queue: "example-queue", false, consumer);//Mesajı tüketecek 
+
+consumer.Received += (sender, e) =>
+{
+    //Kuyruğa gelen mesajın işlendiği yerdir.
+    //e.Body => Kuyruktaki mesajı bütünsel olarak bize getirecektir
+    //e.Body.Span veya e.Body.ToArray() -> Kuyruktak mesajın byte verisini getirecektir
+
+    var message = Encoding.UTF8.GetString(e.Body.Span);
+    Console.WriteLine(message);
+};
+
+Console.Read();
---
+++ RabbitMQ.Publisher/Program.cs
@@ -0,0 +1,32 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+
+//Bağlantı oluşturuldu
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://agynxcdk:czCiBq-QnX_MgyDat8Iqxl2bVtBOGZFH@woodpecker.rmq.cloudamqp.com/agynxcdk");
+
+//Bağlantı Aktifleştirme ve Kanal Açma
+
+using IConnection connection = factory.CreateConnection(); //IDisposable olduğu için using koydum ki kendisini patlatması için.
+using IModel channel = connection.CreateModel(); //IDisposable olduğu için using koydum ki kendisini patlatması için.
+
+//Queue Oluşturm
+channel.QueueDeclare(queue: "example-queue", exclusive: false); //Exclusive mesajların erişilebilmesi için false olarak ayarlanması lazım.Birden fazla channel tarafından bu kodun kullanılabileceğin söyledik.
+
+//Queue mesaj gönderme
+
+//RabbitMQ kuyruğa atacağı mesajları byte türünden kabul etmektedir.Bu yüzden mesajları byte türüne dönüştürmemiz gerekecektir.
+
+
+for (int i = 0; i < 100; i++)
+{
+    byte[] message = Encoding.UTF8.GetBytes("Merhaba" + i);
+
+    channel.BasicPublish(exchange: "", routingKey: "example-queue", body: message); //Defaut exchange : directexchange oldu // routingkey olarak kuyruk adını verdik istediğimizi verebiliriz ve message'ı gödneridk
+}
+
+
+Console.Read();
+
+
