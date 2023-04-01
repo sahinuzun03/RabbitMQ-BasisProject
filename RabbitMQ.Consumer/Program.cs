14b655e6f99f535b12ec621c4e6b97f13efa84ec
uzunsahin <sahin.uzun@gratas.com.tr>
2023-04-02 00:10:49 +0300

    Round-robin , Acknowledgement , BasicNack , BasicQos , Dispatch , Durability konuları ele alındı

--- RabbitMQ.Consumer/Program.cs
+++ RabbitMQ.Consumer/Program.cs
@@ -13,14 +13,15 @@ using IConnection connection = factory.CreateConnection(); //IDisposable olduğu
 using IModel channel = connection.CreateModel(); //IDisposable olduğu için using koydum ki kendisini patlatması için.
 
 //Queue Oluşturm
-channel.QueueDeclare(queue: "example-queue", exclusive: false);
+channel.QueueDeclare(queue: "example-queue", exclusive: false,durable:true);
 //Consumer tarafında da kuyruk aynı yapı ile tanımlanmalıdır.
 
 EventingBasicConsumer consumer = new(channel);
 //Mesajı okumaya dnlemeye başlıyoruz
 //AutoAck mesaj silinecek mi vs
 //consumer en sondaki mesajı tüketecek olan kişi
-channel.BasicConsume(queue: "example-queue", false, consumer);//Mesajı tüketecek 
+channel.BasicConsume(queue: "example-queue", autoAck:false, consumer);//Mesajı tüketecek 
+channel.BasicQos(0, 1, false); // 0 olan prefetchSize : en büyük byte boyutlu mesajı belirtir 0 sınırsız demek // 1 olan prefetchcount aynı anda işleme alınacak olam mesaj sayısı // false değeri ise bu tüm consumerlar için mi yoksa sadece cçağrı yapanlar için mi geçerli onu belirtir.
 
 consumer.Received += (sender, e) =>
 {
@@ -30,6 +31,24 @@ consumer.Received += (sender, e) =>
 
     var message = Encoding.UTF8.GetString(e.Body.Span);
     Console.WriteLine(message);
+
+    channel.BasicAck(e.DeliveryTag, multiple: false);
+
+    //BasicAck burada mesajın başarıyla işlenip işlenmediğini söylemesini sağlayacak
+
+    //multiple birden fazla mesaja dait onay bildirisi gönderir false verilirsa sadece Dbu mesaja onay bilgisi verir
 };
 
 Console.Read();
+
+//Round - Robin Dispatching : mesajların sıralı bir şekilde consumerlara gönderilmesi olayını ıfade etmektedir.
+
+//Message Acknowledgement --> autoAck bunu ayarlıyor. Biz bunu false yaptığımız zaman autoack bunu acknowledgement aktif olmuş oluyor. 
+
+//BasicNack ile işlenmeyen mesajlar geri gönderilebilirler. RabbitMQ'ya bilgi verip meajı tekrardan işletebiliriz. requeue : true olursa mesaj kuyruğa tekrardan işlenmek üzere eklecek false verilir ise kuyruğa eklenmeyecek ve silinecektir.
+
+//BasicCancel ile kuyruktaki bütün mesajların işlenmesinin reddebiliriz.
+
+//BasitReject ile istediğimiz mesajın işlenmesini reddedebilriiz.Burada da requeue parametresi bulumaktadır.
+
+//BasicQos metodu ile mesaj işleme hızını ve teslimat sınrasını belirleyebilriiz.
--- RabbitMQ.Publisher/Program.cs
+++ RabbitMQ.Publisher/Program.cs
@@ -12,10 +12,17 @@ using IConnection connection = factory.CreateConnection(); //IDisposable olduğu
 using IModel channel = connection.CreateModel(); //IDisposable olduğu için using koydum ki kendisini patlatması için.
 
 //Queue Oluşturm
-channel.QueueDeclare(queue: "example-queue", exclusive: false); //Exclusive mesajların erişilebilmesi için false olarak ayarlanması lazım.Birden fazla channel tarafından bu kodun kullanılabileceğin söyledik.
+channel.QueueDeclare(queue: "example-queue", exclusive: false,durable:true); //Exclusive mesajların erişilebilmesi için false olarak ayarlanması lazım.Birden fazla channel tarafından bu kodun kullanılabileceğin söyledik.
+
+//durable : Kuyruk kalıcı hale getirdik
+//Publish edilen mesajarıda kalıcı hale getirmek için
+
+IBasicProperties properties = channel.CreateBasicProperties();
+properties.Persistent = true; 
+
+//Fakat bu yaptığımız durable ve properties mesajların kesin olarak kalıcı olacağını söylemez. Bunun için Outbox / Inbox design pattern kullanmamız gerekebilir.
 
 //Queue mesaj gönderme
-
 //RabbitMQ kuyruğa atacağı mesajları byte türünden kabul etmektedir.Bu yüzden mesajları byte türüne dönüştürmemiz gerekecektir.
 
 
@@ -23,10 +30,11 @@ for (int i = 0; i < 100; i++)
 {
     byte[] message = Encoding.UTF8.GetBytes("Merhaba" + i);
 
-    channel.BasicPublish(exchange: "", routingKey: "example-queue", body: message); //Defaut exchange : directexchange oldu // routingkey olarak kuyruk adını verdik istediğimizi verebiliriz ve message'ı gödneridk
+    channel.BasicPublish(exchange: "", routingKey: "example-queue", body: message,basicProperties:properties); //Defaut exchange : directexchange oldu // routingkey olarak kuyruk adını verdik istediğimizi verebiliriz ve message'ı gödneridk
 }
 
-
 Console.Read();
+
+//Fair Dispatch : Mesajları consumerlara eşit olarak dağıtılmasını sağlamak için kullanılmaktadır.
 
 
